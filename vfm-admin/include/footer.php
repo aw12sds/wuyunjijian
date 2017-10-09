d027868de5d06153a8c1852930aab7dae5b5eb33
aw12sds <aw12dwc1@gmail.com>
2017-10-09 20:23:17 +0800

    first

---
+++ vfm-admin/include/footer.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * VFM - veno file manager: include/footer.php
+ * script footer
+ *
+ * PHP version >= 5.3
+ *
+ * @category  PHP
+ * @package   VenoFileManager
+ * @author    Nicola Franchini <support@veno.it>
+ * @copyright 2013 Nicola Franchini
+ * @license   Exclusively sold on CodeCanyon: http://bit.ly/veno-file-manager
+ * @link      http://filemanager.veno.it/
+ */ ?>
+ <footer class="footer">
+    <div class="container">
+        <span class="pull-left"><a href="http://www.wuyun5.com" target="_blank">
+            乌云网 </a> 
+            &copy; <?php echo date('Y'); ?>
+        </span>
+    </div>
+</footer>
---
+++ vfm-admin/users/users.php
@@ -0,0 +1,10 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$iYMOfzwe$yKXZAFKa0iLhWobcIhKvl1',
+    'role' => 'superadmin',
+  ),
+);
